47c016ed8df73aacd91e367c9dafa71bd666eb56
amin <amin.roosta@outlook.com>
2024-02-10 17:51:43 -0500

    Add node_modules/ and bun.lockb to .gitignore, delete Cargo.toml, add README.md, index.ts, and package.json, modify schema.sql and add tsconfig.json

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TYPE public.oban_job_state AS ENUM (
+CREATE TYPE public.amin_job_state AS ENUM (
     'available',
     'scheduled',
     'executing',
@@ -8,9 +8,9 @@ CREATE TYPE public.oban_job_state AS ENUM (
     'cancelled'
 );
 
-CREATE TABLE public.oban_jobs (
+CREATE TABLE public.amin_jobs (
     id bigint NOT NULL,
-    state public.oban_job_state DEFAULT 'available'::public.oban_job_state NOT NULL,
+    state public.amin_job_state DEFAULT 'available'::public.amin_job_state NOT NULL,
     queue text DEFAULT 'default'::text NOT NULL,
     worker text NOT NULL,
     args jsonb DEFAULT '{}'::jsonb NOT NULL,
@@ -33,12 +33,12 @@ CREATE TABLE public.oban_jobs (
     CONSTRAINT worker_length CHECK (((char_length(worker) > 0) AND (char_length(worker) < 128)))
 );
 
-COMMENT ON TABLE public.oban_jobs IS '12';
-ALTER TABLE public.oban_jobs ADD CONSTRAINT non_negative_priority CHECK ((priority >= 0)) NOT VALID;
-ALTER TABLE ONLY public.oban_jobs ADD CONSTRAINT oban_jobs_pkey PRIMARY KEY (id);
+COMMENT ON TABLE public.amin_jobs IS '12';
+ALTER TABLE public.amin_jobs ADD CONSTRAINT non_negative_priority CHECK ((priority >= 0)) NOT VALID;
+ALTER TABLE ONLY public.amin_jobs ADD CONSTRAINT amin_jobs_pkey PRIMARY KEY (id);
 
 
-CREATE SEQUENCE public.oban_jobs_id_seq
+CREATE SEQUENCE public.amin_jobs_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -46,19 +46,19 @@ CREATE SEQUENCE public.oban_jobs_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.oban_jobs_id_seq OWNED BY public.oban_jobs.id;
-SELECT pg_catalog.setval('public.oban_jobs_id_seq', 1, false);
-ALTER TABLE ONLY public.oban_jobs ALTER COLUMN id SET DEFAULT nextval('public.oban_jobs_id_seq'::regclass);
+ALTER SEQUENCE public.amin_jobs_id_seq OWNED BY public.amin_jobs.id;
+SELECT pg_catalog.setval('public.amin_jobs_id_seq', 1, false);
+ALTER TABLE ONLY public.amin_jobs ALTER COLUMN id SET DEFAULT nextval('public.amin_jobs_id_seq'::regclass);
 
-CREATE UNLOGGED TABLE public.oban_peers (
+CREATE UNLOGGED TABLE public.amin_peers (
     name text NOT NULL,
     node text NOT NULL,
     started_at timestamp without time zone NOT NULL,
     expires_at timestamp without time zone NOT NULL
 );
-ALTER TABLE ONLY public.oban_peers ADD CONSTRAINT oban_peers_pkey PRIMARY KEY (name);
+ALTER TABLE ONLY public.amin_peers ADD CONSTRAINT amin_peers_pkey PRIMARY KEY (name);
 
-CREATE INDEX oban_jobs_args_index ON public.oban_jobs USING gin (args);
-CREATE INDEX oban_jobs_meta_index ON public.oban_jobs USING gin (meta);
-CREATE INDEX oban_jobs_state_queue_priority_scheduled_at_id_index ON public.oban_jobs USING btree (state, queue, priority, scheduled_at, id);
+CREATE INDEX amin_jobs_args_index ON public.amin_jobs USING gin (args);
+CREATE INDEX amin_jobs_meta_index ON public.amin_jobs USING gin (meta);
+CREATE INDEX amin_jobs_state_queue_priority_scheduled_at_id_index ON public.amin_jobs USING btree (state, queue, priority, scheduled_at, id);
 
