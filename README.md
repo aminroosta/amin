002b658ac74c9459949ace35f6a9ec42883f3af9
amin <amin.roosta@outlook.com>
2024-02-18 16:27:51 -0500

    thinking about database integration

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Eram
 
+- https://typedoc.org
+- https://github.com/nestjs/docs.nestjs.com
 Telemetry:
   * `{"module": "eram", "event": "job", "action": "start"}` — at the point a job is fetched from the database and will execute
   * `{"module": "eram", "event": "job", "action": "stop"}` — after a job succeeds and the success is recorded in the database
   * `{"module": "eram", "event": "job", "action": "exception"}` — after a job fails and the failure is recorded in the database
   * `{"module": "eram", "event": "worker", "action": "init"}` - when the Eram worker is started this will execute
+
+
--- index.ts
+++ index.ts
@@ -1,1 +1,66 @@
 console.log("Hello via Bun!");
+
+var eram = {} as any;
+const emailUserJob = eram.registerJob('EamilUserJob', ({ email, rank }: { email: string, rank: number }) => {
+  console.log({ email, rank });
+});
+
+
+emailUserJob.enqueue({ email: 'a@b.c', rank: 5 });
+
+interface IJob<T = {}> {
+  perform(args: T): any;
+}
+
+
+class EmailUserJob {
+  static {
+    eram.register(EmailUserJob, {
+      queue: 'emails',
+      priority: 3,
+      max_attempts: 3,
+      tags: ["business"],
+      unique: { period: 30 }
+    });
+  }
+
+  // static enqueue(email: string, rank: number) {
+  //   eram.enqueueJob(EmailUserJob, { email, rank });
+  // }
+
+  async perform({ email, rank }: { email: string, rank: number }) {
+    console.log({ email, rank });
+  }
+}
+
+
+
+// eram.enqueue(new EmailUserJob({ email: 'a@b.c', rank: 5 }));
+
+eram.enqueue(EmailUserJob, { email: 'a@b.c', rank: 5 });
+
+
+
+/* pg */
+var client = {} as any;
+await client.query('SELECT $1::text as message', ['Hello world!'])
+client.query('LISTEN foo');
+client.on('notification', (msg: any) => { });
+
+// client = await pool.connect()
+// try {
+//   await client.query('BEGIN')
+//   // ...
+//   await client.query('COMMIT')
+// } catch (e) {
+//   await client.query('ROLLBACK')
+//   throw e
+// } finally {
+//   client.release()
+// }
+
+// postgres.js
+var sql = {} as any;
+await sql`SELECT ${'hello world!'} as message`;
+await sql.listen('news', payload => { });
+sql.notify('news', "...");
